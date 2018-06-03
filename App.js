61b702b782a9b966ac96012dbe87865bbd1b0195
“zan768616253” <768616253@qq.com>
2018-06-03 12:32:37 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import InstaClone from './src/InstaClone.js'
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <InstaClone />
+    );
+  }
+}
